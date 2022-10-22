f0e02b920c402d9b3f7fbed98a170d5a73c1df30
LucasFranca0 <lucascfranca11@gmail.com>
2022-10-22 10:29:32 -0300

    update UsuarioModel

--- aula-rest-api/src/main/java/estudosAPI/aula/rest/api/controller/model/UsuarioModel.java
+++ aula-rest-api/src/main/java/estudosAPI/aula/rest/api/controller/model/UsuarioModel.java
@@ -1,9 +1,13 @@
 package estudosAPI.aula.rest.api.controller.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
+@Getter
+@Setter
 @Entity(name = "usuario")
 public class UsuarioModel {
 
@@ -12,40 +16,11 @@ public class UsuarioModel {
 
     @Column(nullable = false, length = 50)
     public String name;
+
     @Column(nullable = false, length = 50)
     public String login;
+
     @Column(nullable = false, length = 10)
     public String senha;
 
-    public Integer getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(Integer codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 }
